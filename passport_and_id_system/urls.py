69a2866b9e9c7d37e077d5fdd94871f0397bd8e8
patrice <chaulapatrice@gmail.com>
2024-12-12 14:00:17 +0200

    chore: configure urls

--- passport_and_id_system/urls.py
+++ passport_and_id_system/urls.py
@@ -20,8 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("core.urls"))
+    path("", admin.site.urls),
+    path("portal", include("core.urls"))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
